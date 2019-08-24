bd10ad91b8474bd9b8d6d50e5b5ff4399ec74ab9
ykliu <moneyflying_2006@hotmail.com>
2019-08-24 19:11:35 +0800

    done

--- src/LearnScala/BuiltinControlStructures.scala
+++ src/LearnScala/BuiltinControlStructures.scala
@@ -19,11 +19,17 @@ object BuiltinControlStructures {
     def fileLines(file: java.io.File) =
       scala.io.Source.fromFile(file).getLines().toList
 
+//    def grep(pattern: String) =
+//      for {
+//        file <- filesHere if file.getName.endsWith(".scala")
+//        line <- fileLines(file) if line.trim.matches(pattern)
+//      } println(file + ": " + line.trim)
     def grep(pattern: String) =
       for {
         file <- filesHere if file.getName.endsWith(".scala")
-        line <- fileLines(file) if line.trim.matches(pattern)
-      } println(file + ": " + line.trim)
+        line <- fileLines(file)
+        trimmed = line.trim if trimmed.matches(pattern)
+      } println(file + ": " + trimmed)
 
     grep(".*gcd.*")
   }
