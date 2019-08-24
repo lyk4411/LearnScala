93dc935f591d98c83ebd9bbeee44d1b8a1d9b65f
ykliu <moneyflying_2006@hotmail.com>
2019-08-24 11:40:28 +0800

    done

---
+++ src/LearnScala/BuiltinControlStructures.scala
@@ -0,0 +1,32 @@
+package LearnScala
+
+/**
+  * Created by lyk on 2019-8-24.
+  * Package name: LearnScala
+  * Porject name: LearnScala
+  */
+object BuiltinControlStructures {
+  def main(args: Array[String]) {
+    val filesHere = (new java.io.File("./src/LearnScala")).listFiles
+
+//    for (
+//      file <- filesHere
+//      if file.isFile
+//      if file.getName.endsWith(".scala")
+//    ) println(file)
+
+
+    def fileLines(file: java.io.File) =
+      scala.io.Source.fromFile(file).getLines().toList
+
+    def grep(pattern: String) =
+      for (
+        file <- filesHere
+        if file.getName.endsWith(".scala");
+        line <- fileLines(file)
+        if line.trim.matches(pattern)
+      ) println(file + ": " + line.trim)
+
+    grep(".*gcd.*")
+  }
+}
