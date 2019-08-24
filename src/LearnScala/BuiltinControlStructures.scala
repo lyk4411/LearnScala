21ea1bd38c3691395125421a73293892fe11d410
ykliu <moneyflying_2006@hotmail.com>
2019-08-24 19:56:09 +0800

    done

--- src/LearnScala/BuiltinControlStructures.scala
+++ src/LearnScala/BuiltinControlStructures.scala
@@ -32,5 +32,32 @@ object BuiltinControlStructures {
       } println(file + ": " + trimmed)
 
     grep(".*gcd.*")
+
+    def scalaFiles =
+      for {
+        file <- filesHere
+        if file.getName.endsWith(".scala")
+      } yield file
+    for(i <- 0 until scalaFiles.length)
+      println(i + ":" + scalaFiles(i))
+
+    val forLineLengths =
+      for {
+        file <- filesHere
+        if file.getName.endsWith(".scala")
+        line <- fileLines(file)
+        trimmed = line.trim
+        if trimmed.matches(".*for.*")
+      } yield trimmed.length
+
+    for(i <- 0 until forLineLengths.length)
+      println(i + ":" + forLineLengths(i))
+
+
+    def f(): Int = try return 1 finally return 2
+    def g(): Int = try 1 finally 2
+    println("f():" + f())
+    println("g():" + g())
+
   }
 }
