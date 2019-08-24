056be252e557240317725a7fd80773d9199bfd39
ykliu <moneyflying_2006@hotmail.com>
2019-08-24 08:52:55 +0800

    Initial commit

---
+++ src/HelloWorld.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by lyk on 2019-8-24.
+  * Package name: 
+  * Porject name: LearnScala
+  */
+object HelloWorld {
+  def main (args: Array[String] ) {
+    print("hello world.")
+  }
+}
