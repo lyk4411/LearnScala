951f0868e012ae8160951d71356d413f88ee2499
ykliu <moneyflying_2006@hotmail.com>
2019-08-29 16:58:57 +0800

    done

--- src/LearnScala/ex07.scala
+++ src/LearnScala/ex07.scala
@@ -30,9 +30,31 @@ object  ex07 {
     }
     // i still in scope; j, prod, and k out of scope
   }
+  def makeRowSeq(row: Int) =
+    for (col <- 1 to 10) yield {
+      val prod = (row * col).toString
+      val padding = " " * (4 - prod.length)
+      padding + prod
+    }
+
+  // Returns a row as a string
+  def makeRow(row: Int) = makeRowSeq(row).mkString
+
+  // Returns table as a string with one row per line
+  def multiTable() = {
+
+    val tableSeq = // a sequence of row strings
+      for (row <- 1 to 10)
+        yield makeRow(row)
+
+    tableSeq.mkString("\n")
+  }
 
   def main(args: Array[String]) {
-    val a = printMultiTable
-    a
+    printMultiTable
+
+    print("==============================================")
+    val b = multiTable
+    print(b)
   }
 }
