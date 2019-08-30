e5d6fcdaa99b298e0256975847425f9445cb68f4
ykliu <moneyflying_2006@hotmail.com>
2019-08-30 15:46:47 +0800

    done

---
+++ src/LearnScala/ex11.scala
@@ -0,0 +1,14 @@
+package LearnScala
+
+/**
+  * Created by lyk on 2019-8-30.
+  * Package name: LearnScala
+  * Porject name: LearnScala
+  */
+object ex11 {
+  def main(args: Array[String]) {
+    println(42 max 43)
+    println(42.max(43))
+
+  }
+}
