227af521bb93d8a4428c938b7997e29f0ed359bb
ykliu <moneyflying_2006@hotmail.com>
2019-09-11 16:52:05 +0800

    done

---
+++ src/LearnScala/ex21.scala
@@ -0,0 +1,30 @@
+package LearnScala
+
+/**
+  * Created by lyk on 2019-9-11.
+  * Package name: LearnScala
+  * Porject name: LearnScala
+  */
+object ex21 {
+//  implicit def intToString(x: Int) = x.toString
+//  implicit def doubleToInt(x: Double) = x.toInt
+//  implicit def int2double(x: Int): Double = x.toDouble
+  implicit def intToRational(x: Int) = new Rational(x)
+
+  def main(args: Array[String]) {
+//    val i: Int = 3.5
+//    println(i)
+    val oneHalf = new Rational(1, 2)
+//    println(oneHalf + 2)
+    println(2 + oneHalf)
+    println(new Rational(2) + oneHalf)
+    println(new Rational(2, 1) + oneHalf)
+
+
+  }
+}
+
+case class Rectangle(width: Int, height: Int)
+class RectangleMaker(width: Int) {
+  def x(height: Int) = Rectangle(width, height)
+}
