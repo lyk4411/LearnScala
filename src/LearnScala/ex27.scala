3b53cc33ced28c42a2c33fca55caa6b6d687cf68
ykliu <moneyflying_2006@hotmail.com>
2019-09-17 14:57:24 +0800

    done

---
+++ src/LearnScala/ex27.scala
@@ -0,0 +1,12 @@
+package LearnScala
+import annotation._
+/**
+  * Created by lyk on 2019-9-17.
+  * Package name: LearnScala
+  * Porject name: LearnScala
+  */
+object ex27 {
+  class strategy(arg: Annotation) extends Annotation
+  class delayed extends Annotation
+  
+}
