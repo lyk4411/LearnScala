0e219b62075ec84fa2fd7fc21fbd6419e3e13f47
ykliu <moneyflying_2006@hotmail.com>
2019-09-23 10:54:04 +0800

    done

---
+++ src/LearnScala/FindMinimuminRotatedSortedArray.scala
@@ -0,0 +1,35 @@
+package LearnScala
+
+/**
+  * Created by lyk on 2019-9-23.
+  * Package name: LearnScala
+  * Porject name: LearnScala
+  */
+object FindMinimuminRotatedSortedArray {
+  def findMin(nums: Array[Int]): Int = {
+    if(nums == null || nums.length == 0)
+      return 0
+    if(nums.length == 1)
+      return nums(0)
+    var start:Int = 0
+    var end:Int = nums.length - 1
+    while(start < end){
+      var mid:Int = (start + end) / 2
+      if(mid > 0 && nums(mid) < nums(mid - 1)) {
+        return nums(mid)
+      }
+      if (nums(start) <= nums(mid) && nums(mid) > nums(end)) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+    return nums(start);
+  }
+
+  def main(args: Array[String]) {
+    println(findMin(Array(0,1,2,4,5,6,7)))
+    println(findMin(Array(3,4,5,1,2)))
+    println(findMin(Array(4,5,6,7,0,1,2)))
+  }
+}
