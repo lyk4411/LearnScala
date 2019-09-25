cd8ebaa6df9c16613afd82c909edcfb0a3cc289a
ykliu <moneyflying_2006@hotmail.com>
2019-09-25 16:16:30 +0800

    done

--- src/Leetcode/TrappingRainWater.scala
+++ src/Leetcode/TrappingRainWater.scala
@@ -8,28 +8,28 @@ import scala.collection.mutable
   * Porject name: LearnScala
   */
 class TrappingRainWater {
-//  def trap(height: Array[Int]): Int = {
-//    val stack = new mutable.Stack[Int]()
-//    var waterTrapped = 0
-//
-//    for (i <- 0 to height.size - 1) {
-//      while (!stack.isEmpty && height(i) > height(stack.top)) { // With water trapped.
-//        val bottomHeight = height(stack.pop())
-//          if (!stack.isEmpty) {
-//            val leftHeight = height(stack.top)
-//            val width = i - stack.top - 1
-//            waterTrapped += width * (Math.min(leftHeight, height(i)) - bottomHeight)
-//          }
-//      }
-//      stack.push(i)
-//    }
-//    waterTrapped
-//  }
+  def trap(height: Array[Int]): Int = {
+    val stack = new mutable.Stack[Int]()
+    var waterTrapped = 0
 
-  def trap(height: Array[Int]): Int =
-    height.scan(0)(_ max _).drop(1)
-      .zip(height.scanRight(0)(_ max _).dropRight(1))
-      .map(Function.tupled(_ min _)).sum - height.sum
+    for (i <- 0 to height.size - 1) {
+      while (!stack.isEmpty && height(i) > height(stack.top)) { // With water trapped.
+        val bottomHeight = height(stack.pop())
+          if (!stack.isEmpty) {
+            val leftHeight = height(stack.top)
+            val width = i - stack.top - 1
+            waterTrapped += width * (Math.min(leftHeight, height(i)) - bottomHeight)
+          }
+      }
+      stack.push(i)
+    }
+    waterTrapped
+  }
+
+//  def trap(height: Array[Int]): Int =
+//    height.scan(0)(_ max _).drop(1)
+//      .zip(height.scanRight(0)(_ max _).dropRight(1))
+//      .map(Function.tupled(_ min _)).sum - height.sum
 }
 object TrappingRainWater {
   def main(args: Array[String]) {
