3591e9d4a377ee74f16ebb8a4294a1a2d74979ee
ykliu <moneyflying_2006@hotmail.com>
2019-09-27 16:04:15 +0800

    done

--- src/Leetcode/BestTimetoBuyandSellStock.scala
+++ src/Leetcode/BestTimetoBuyandSellStock.scala
@@ -7,10 +7,14 @@ package Leetcode
   */
 class BestTimetoBuyandSellStock {
   def maxProfit(prices: Array[Int]): Int = {
-    if(prices == null || prices.length ==0) 0
-    else prices.foldLeft(prices.head,0)((pair:(Int, Int), p)=> {
-      (Math.min(p, pair._1), Math.max(pair._2, p - pair._1) )
-    })._2
+//    if(prices == null || prices.length ==0) 0
+//    else prices.foldLeft(prices.head,0)((pair:(Int, Int), p)=> {
+//      (Math.min(p, pair._1), Math.max(pair._2, p - pair._1) )
+//    })._2
+    println(prices.scan(Int.MaxValue)(math.min).tail.toList)
+    println(prices.zip(prices.scan(Int.MaxValue)(math.min).tail).toList)
+    println(prices.zip(prices.scan(Int.MaxValue)(math.min).tail).map(p=>p._1 - p._2).toList)
+    prices.zip(prices.scan(Int.MaxValue)(math.min).tail).map(p => p._1 - p._2).foldLeft(0)(math.max)
   }
 }
 object BestTimetoBuyandSellStock {
