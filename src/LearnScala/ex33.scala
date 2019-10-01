cfe9085209af8d4ddcd2fba1e4f7969003528692
ykliu <moneyflying_2006@hotmail.com>
2019-10-01 08:33:10 +0800

    done

--- src/LearnScala/ex33.scala
+++ src/LearnScala/ex33.scala
@@ -22,3 +22,26 @@ object ParseExpr extends Arith {
     println(parseAll(expr, args(0)))
   }
 }
+
+object MyParsers extends RegexParsers {
+  val ident: Parser[String] = """[a-zA-Z_]\w*""".r
+}
+
+import scala.util.parsing.combinator._
+class JSON extends JavaTokenParsers {
+  def value : Parser[Any] = obj | arr |
+    stringLiteral |
+    floatingPointNumber |
+    "null" | "true" | "false"
+  def obj   : Parser[Any] = "{"~repsep(member, ",")~"}"
+  def arr   : Parser[Any] = "["~repsep(value, ",")~"]"
+  def member: Parser[Any] = stringLiteral~":"~value
+}
+import java.io.FileReader
+
+object ParseJSON extends JSON {
+  def main(args: Array[String]) = {
+    val reader = new FileReader(args(0))
+    println(parseAll(value, reader))
+  }
+}
