72ebac979aa765278d30125a8ad6d0babb9f028b
ykliu <moneyflying_2006@hotmail.com>
2019-10-01 21:34:11 +0800

    done

--- src/FunctionalProgrammingInScala/answerkey/streamingio/07.hint.scala
+++
@@ -1,3 +0,0 @@
-package FunctionalProgrammingInScala.answerkey.streamingio
-
--
---
+++ src/FunctionalProgrammingInScala/project/Tree.scala
@@ -0,0 +1,13 @@
+package FunctionalProgrammingInScala.project
+
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+
+object Tree {
+
+
+
+
+}
