fcc3b4c1dfa312feccde99ff11c3f16248c41551
ykliu <moneyflying_2006@hotmail.com>
2019-10-23 10:34:55 +0800

    done

--- src/Leetcode/KthLargestElementinanArray.scala
+++ src/Leetcode/KthLargestElementinanArray.scala
@@ -6,24 +6,24 @@ package Leetcode
   * Porject name: LearnScala
   */
 class KthLargestElementinanArray {
-  def findKthLargest(nums: Array[Int], k: Int): Int = {
-    var lowest = Int.MinValue
-    val pq = scala.collection.mutable.PriorityQueue.empty[Int](Ordering.by(_ * -1))
-    nums.foreach { num =>
-      if (lowest < num) pq.enqueue(num)
-      if (pq.size > k) lowest = pq.dequeue
-    }
-    pq.dequeue
-  }
-//    def findKthLargest(nums: Array[Int], k: Int): Int = {
-//      nums
-//        .drop(k)
-//        .foldLeft(nums.take(k).sorted)((sortedList, current) => {
-//          if (current > sortedList.head) (sortedList.drop(1) :+ current).sorted
-//          else sortedList
-//        })
-//        .head
+//  def findKthLargest(nums: Array[Int], k: Int): Int = {
+//    var lowest = Int.MinValue
+//    val pq = scala.collection.mutable.PriorityQueue.empty[Int](Ordering.by(_ * -1))
+//    nums.foreach { num =>
+//      if (lowest < num) pq.enqueue(num)
+//      if (pq.size > k) lowest = pq.dequeue
 //    }
+//    pq.dequeue
+//  }
+    def findKthLargest(nums: Array[Int], k: Int): Int = {
+      nums
+        .drop(k)
+        .foldLeft(nums.take(k).sorted)((sortedList, current) => {
+          if (current > sortedList.head) (sortedList.drop(1) :+ current).sorted
+          else sortedList
+        })
+        .head
+    }
 }
 object KthLargestElementinanArray {
   def main(args: Array[String]) {
