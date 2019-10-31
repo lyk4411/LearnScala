a6e1f1c6210b179490dfb7af433fe1a9397b0466
ykliu <moneyflying_2006@hotmail.com>
2019-10-31 15:20:29 +0800

    done

--- src/Leetcode/ValidSudoku.scala
+++ src/Leetcode/ValidSudoku.scala
@@ -6,41 +6,40 @@ package Leetcode
   * Porject name: LearnScala
   */
 class ValidSudoku {
-  def isValidSudoku(board: Array[Array[Char]]): Boolean = isValidRows(board) && isValidCols(board) && isValidSubBoxes(board)
-  def isValidRows(board: Array[Array[Char]]): Boolean = !board.exists(row => containsDuplicate(row.filter(_.isDigit)))
-  def isValidCols(board: Array[Array[Char]]): Boolean = isValidRows(board.transpose)
-  def isValidSubBoxes(board: Array[Array[Char]]): Boolean = {
-    !(0 until 3).exists(xMultiplier => {
-      val sliceFrom = xMultiplier * 3
-      val sliceUntil = sliceFrom + 3
-      (0 until 3).exists(yMultiplier => {
-        val subBox = board(yMultiplier * 3).slice(sliceFrom, sliceUntil) ++
-          board(yMultiplier * 3 + 1).slice(sliceFrom, sliceUntil) ++
-          board(yMultiplier * 3 + 2).slice(sliceFrom, sliceUntil)
-        containsDuplicate(subBox.filter(_.isDigit))
-      })
-    })
-  }
-  private def containsDuplicate[T](a: Array[T]): Boolean = {
-    val set = new scala.collection.mutable.HashSet[T]()
-    !a.forall(set.add)
-  }
-
-//  def initHM() = (0 to 8).map((_, scala.collection.mutable.Set.empty[Char])).toMap
-//  def isValidSudoku(board: Array[Array[Char]]): Boolean = {
-//    val rowHM = initHM()
-//    val colHM = initHM()
-//    val boxHM = initHM()
-//    (0 to 8).foreach { rowIdx =>
-//      (0 to 8).collect { case colIdx if board(rowIdx)(colIdx) != '.' =>
-//        val num = board(rowIdx)(colIdx)
-//        val boxNum = ((rowIdx / 3) * 3)  + ((colIdx) / 3)
-//
-//        if (!rowHM(rowIdx).add(num) || !colHM(colIdx).add(num) || !boxHM(boxNum).add(num)) return false
-//      }
-//    }
-//    true
+//  def isValidSudoku(board: Array[Array[Char]]): Boolean = isValidRows(board) && isValidCols(board) && isValidSubBoxes(board)
+//  def isValidRows(board: Array[Array[Char]]): Boolean = !board.exists(row => containsDuplicate(row.filter(_.isDigit)))
+//  def isValidCols(board: Array[Array[Char]]): Boolean = isValidRows(board.transpose)
+//  def isValidSubBoxes(board: Array[Array[Char]]): Boolean = {
+//    !(0 until 3).exists(xMultiplier => {
+//      val sliceFrom = xMultiplier * 3
+//      val sliceUntil = sliceFrom + 3
+//      (0 until 3).exists(yMultiplier => {
+//        val subBox = board(yMultiplier * 3).slice(sliceFrom, sliceUntil) ++
+//          board(yMultiplier * 3 + 1).slice(sliceFrom, sliceUntil) ++
+//          board(yMultiplier * 3 + 2).slice(sliceFrom, sliceUntil)
+//        containsDuplicate(subBox.filter(_.isDigit))
+//      })
+//    })
 //  }
+//  private def containsDuplicate[T](a: Array[T]): Boolean = {
+//    val set = new scala.collection.mutable.HashSet[T]()
+//    !a.forall(set.add)
+//  }
+
+  def initHM() = (0 to 8).map((_, scala.collection.mutable.Set.empty[Char])).toMap
+  def isValidSudoku(board: Array[Array[Char]]): Boolean = {
+    val rowHM = initHM()
+    val colHM = initHM()
+    val boxHM = initHM()
+    (0 to 8).foreach { rowIdx =>
+      (0 to 8).collect { case colIdx if board(rowIdx)(colIdx) != '.' =>
+        val num = board(rowIdx)(colIdx)
+        val boxNum = ((rowIdx / 3) * 3)  + ((colIdx) / 3)
+        if (!rowHM(rowIdx).add(num) || !colHM(colIdx).add(num) || !boxHM(boxNum).add(num)) return false
+      }
+    }
+    true
+  }
 }
 object ValidSudoku {
   def main(args: Array[String]) {
