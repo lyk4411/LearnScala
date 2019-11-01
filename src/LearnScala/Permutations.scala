deda5b37ac99e1bf24fe26c1be2c9849ee7f3af7
ykliu <moneyflying_2006@hotmail.com>
2019-11-01 14:37:24 +0800

    done

---
+++ src/LearnScala/Permutations.scala
@@ -0,0 +1,32 @@
+package LearnScala
+
+/**
+  * Created by lyk on 2019-11-1.
+  * Package name: LearnScala
+  * Porject name: LearnScala
+  */
+class Permutations {
+  def permute(nums: Array[Int]): List[List[Int]] = {
+    def permute(rem: Set[Int]): Set[List[Int]] =
+      if (rem.isEmpty) Set(Nil)
+      else rem flatMap (n => permute(rem - n) map (n :: _))
+    permute(nums.toSet).toList
+  }
+
+//  def permute(nums: Array[Int]): List[List[Int]] = {
+//    if (nums.isEmpty) {
+//      List(List())
+//    } else {
+//      for {
+//        x <- nums.toList
+//        perm <- permute(nums.filter(_ != x))
+//      } yield x :: perm
+//    }
+//  }
+}
+object Permutations {
+  def main(args: Array[String]) {
+    val a = new Permutations
+    println(a.permute(Array(1, 2, 3)))
+  }
+}
