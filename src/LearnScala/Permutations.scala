6390385ae94a6945226fc594455b0487398ece60
ykliu <moneyflying_2006@hotmail.com>
2019-11-01 14:49:24 +0800

    done

--- src/LearnScala/Permutations.scala
+++ src/LearnScala/Permutations.scala
@@ -13,6 +13,10 @@ class Permutations {
     permute(nums.toSet).toList
   }
 
+  def permute(rem: Set[Int]): Set[List[Int]] =
+    if (rem.isEmpty) Set(Nil)
+    else rem flatMap (n => permute(rem - n) map (n :: _))
+
 //  def permute(nums: Array[Int]): List[List[Int]] = {
 //    if (nums.isEmpty) {
 //      List(List())
@@ -28,5 +32,7 @@ object Permutations {
   def main(args: Array[String]) {
     val a = new Permutations
     println(a.permute(Array(1, 2, 3)))
+    println(a.permute(Set(1, 2, 3)))
+
   }
 }
