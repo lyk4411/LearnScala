3ed84cc11fc76ea37d1bafa0df538cafa863ec98
ykliu <moneyflying_2006@hotmail.com>
2019-11-15 18:24:19 +0800

    done

---
+++ src/Leetcode/NextGreaterElementI.scala
@@ -0,0 +1,34 @@
+package Leetcode
+
+import scala.collection.mutable
+
+/**
+  * Created by lyk on 2019-11-15.
+  * Package name: Leetcode
+  * Porject name: LearnScala
+  */
+class NextGreaterElementI {
+  def nextGreaterElement(findNums: Array[Int], nums: Array[Int]): Array[Int] = {
+    var map = Map.empty[Int, Int]
+    val stack = mutable.Stack[Int]()
+    var arr = Array.empty[Int]
+    for (num <- nums) {
+      if (stack.nonEmpty && num > stack.top) {
+        map += stack.top -> num
+      }
+      stack.push(num)
+    }
+    for (i <- findNums) {
+      arr :+= map.getOrElse(i, -1)
+    }
+    arr
+  }
+}
+object NextGreaterElementI {
+  def main(args: Array[String]) {
+    val a = new NextGreaterElementI
+    a.nextGreaterElement(Array(4,1,2), Array(1,3,4,2)).foreach(n => print(n, " "))
+    println
+    a.nextGreaterElement(Array(2, 4), Array(1,2,3,4)).foreach(n => print(n, " "))
+  }
+}
